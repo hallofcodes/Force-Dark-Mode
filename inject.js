d3d0907e28ec980e28ae77d422c57c862fb7b6ea
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2022-11-05 22:24:54 +0100

    added Support for emojis. This is to prevent emojis from being inverted in the opposite way.

--- inject.js
+++ inject.js
@@ -21,18 +21,18 @@ javascript: (
                   '-moz-filter: invert(100%);' + 
                   '-o-filter: invert(100%);' + 
                   '-ms-filter: invert(100%);}' +
-                  'img, iframe, video, canvas, svg, picture {-webkit-filter: invert(100%) !important;' +
+                  'img, iframe, video, canvas, svg, picture , .webvium-pictograph-class {-webkit-filter: invert(100%) !important;' +
                   '-moz-filter: invert(100%) !important;' + 
                   '-o-filter: invert(100%) !important;' + 
                   '-ms-filter: invert(100%) !important;}',
-        head = document.getElementsByTagName('head')[0], style = document.createElement('style');
+        head = document.getElementsByTagName('head')[0], style = document.createElement('style') , body = document.getElementsByTagName('body')[0];
         if (!window.counter) { 
             window.counter = 1;
         } else { 
             window.counter ++;
             if (window.counter % 2 == 0) { 
                 var css = 'html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' +
-                    'img, iframe, video, canvas, svg, picture {-webkit-filter: invert(0%) !important; -moz-filter: invert(0%) !important; -o-filter: invert(0%) !important; -ms-filter: invert(0%) !important;}'
+                    'img, iframe, video, canvas, svg, picture , .webvium-pictograph-class {-webkit-filter: invert(0%) !important; -moz-filter: invert(0%) !important; -o-filter: invert(0%) !important; -ms-filter: invert(0%) !important;}'
             }
         };
         style.type = 'text/css';
@@ -42,5 +42,33 @@ javascript: (
             style.appendChild(document.createTextNode(css));
         }
         head.appendChild(style);
+        function rewriteString (string) {
+            const regex = /\p{Extended_Pictographic}/ug;
+            
+            const indexes = [];
+            const match = string.match(regex);
+            if(match) {
+                const matchLength = match.length;
+                
+                const newString = Array.from(string);
+                
+                while(indexes.length < matchLength ) {
+                    if(indexes.length === 0) {
+                        indexes.push(string.search(regex));
+                    }
+                    else {
+                        indexes.push(string.substring(indexes[indexes.length - 1] + 1).search(regex) + indexes[indexes.length - 1] + 1);
+                    }
+                }
+                for(let i = 0; i < matchLength; i++) {
+                    newString[indexes[i]] = `<span class="webvium-pictograph-class">${match[i]}</span>`;
+                }
+                return newString.join("")
+            }
+            else {
+                return string
+            }
+        }
+        body.innerHTML = rewriteString(body.innerHTML);
     }
 ());
